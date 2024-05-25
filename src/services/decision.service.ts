f96d4f23c416c05994871ee7c56b28b4b299052b
Pierre-Henry Soria <pierre@ph7.me>
2024-05-25 23:19:25 +1000

    Use enum to store local storage

--- src/services/decision.service.ts
+++ src/services/decision.service.ts
@@ -1,13 +1,12 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Decision } from "../types/decision.type";
-
-const DECISION_STORAGE_KEY = "decisions";
+import { LocalStorageKeys } from "../types/localStorageKeys";
 
 export const saveDecision = async (decision: Decision) => {
   try {
     const existingDecisions = await getDecisions();
     const updatedDecisions = [...existingDecisions, decision];
-    await AsyncStorage.setItem(DECISION_STORAGE_KEY, JSON.stringify(updatedDecisions));
+    await AsyncStorage.setItem(LocalStorageKeys.DECISION, JSON.stringify(updatedDecisions));
   } catch (error) {
     console.error("Error saving decision:", error);
     throw error;
@@ -16,7 +15,7 @@ export const saveDecision = async (decision: Decision) => {
 
 export const getDecisions = async (): Promise<Decision[]> => {
   try {
-    const decisionsString = await AsyncStorage.getItem(DECISION_STORAGE_KEY);
+    const decisionsString = await AsyncStorage.getItem(LocalStorageKeys.DECISION);
     return decisionsString ? JSON.parse(decisionsString) : [];
   } catch (error) {
     console.error("Error retrieving decisions:", error);
---
+++ src/types/localStorageKeys.ts
@@ -0,0 +1,3 @@
+export enum LocalStorageKeys {
+  DECISION = "@localStorage:decisions",
+}
