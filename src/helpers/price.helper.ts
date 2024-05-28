8b7996bdedc1cac134bd22ac3007bdfc9a472f17
Pierre-Henry Soria <pierre@ph7.me>
2024-05-28 20:54:21 +1000

    remove unused files

--- src/helpers/price.helper.ts
+++
@@ -1,6 +0,0 @@
-export const formatPrice = (price: number): string =>
-  new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    maximumFractionDigits: 0, // remove trailing zeros (cents)
-  }).format(price);
