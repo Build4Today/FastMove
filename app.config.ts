038789401091f73c40c3aedb0f8043fd9c18e026
Pierre-Henry Soria <pierre@ph7.me>
2024-06-08 20:50:12 +1000

    Make app slug name be consistent with app name ID/package name and avoid confusion

--- app.config.ts
+++ app.config.ts
@@ -8,10 +8,10 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     name: APP_ENV === "production" ? "FastMove" : "FastMove (DEV)",
     slug: APP_ENV === "production" ? "fast-move" : "fast-move-dev",
     ios: {
-      bundleIdentifier: APP_ENV === "production" ? "me.ph7.fastmove" : "dev.ph7.fastmove-dev",
+      bundleIdentifier: APP_ENV === "production" ? "me.ph7.fast-move" : "dev.ph7.fast-move-dev",
     },
     android: {
-      package: APP_ENV === "production" ? "me.ph7.fastmove" : "dev.ph7.fastmove.dev",
+      package: APP_ENV === "production" ? "me.ph7.fast-move" : "dev.ph7.fast-move.dev",
     },
     extra: {
       eas: {
