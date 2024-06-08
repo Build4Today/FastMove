f8bc1923c2394496777806e7a75c4cc4db10356e
Pierre-Henry Soria <pierre@ph7.me>
2024-06-08 18:15:33 +1000

    Prepare auto-submit release

--- app.config.js
+++ app.config.js
@@ -1,12 +1,8 @@
 export default () => {
-    return {
-      name: process.env.APP_ENV === "production" ? "Life Routine" : "Life Routine (DEV)",
-      ios: {
-        bundleIdentifier:
-          process.env.APP_ENV === "production"
-            ? "me.ph7.life-routine"
-            : "dev.ph7.life-routine-dev",
-      },
-    };
+  return {
+    name: process.env.APP_ENV === "production" ? "Life Routine" : "Life Routine (DEV)",
+    ios: {
+      bundleIdentifier: process.env.APP_ENV === "production" ? "me.ph7.fastmove" : "dev.ph7.fastmove-dev",
+    },
   };
-  
+};
