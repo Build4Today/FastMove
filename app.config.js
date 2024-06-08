a1d7012e8b908ce7253ffc4e8f1c4a98a8c05c27
Pierre-Henry Soria <pierre@ph7.me>
2024-06-08 18:06:31 +1000

    Set environment dev/prod

---
+++ app.config.js
@@ -0,0 +1,12 @@
+export default () => {
+    return {
+      name: process.env.APP_ENV === "production" ? "Life Routine" : "Life Routine (DEV)",
+      ios: {
+        bundleIdentifier:
+          process.env.APP_ENV === "production"
+            ? "me.ph7.life-routine"
+            : "dev.ph7.life-routine-dev",
+      },
+    };
+  };
+  
